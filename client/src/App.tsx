e1e3d79cd99efb5d9d05f58dbbea13cab1f4485c
CqzShepie <sheppardthomashenry@outlook.com>
2026-06-30 13:02:45 +0100

    Add customer team page switch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
 import AcceptStaffInvite from "./pages/AcceptStaffInvite";
+import CustomerStaff from "./pages/CustomerStaff";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
   if (window.location.pathname === "/accept-staff-invite") {
     return <AcceptStaffInvite />;
+  }
+
+  if (window.location.pathname === "/team") {
+    return <CustomerStaff />;
   }
 
   return <AppRouter />;
