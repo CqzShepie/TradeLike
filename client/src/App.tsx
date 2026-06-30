c978840a8fb2bff6dea9279bf863bc08014c3d91
CqzShepie <sheppardthomashenry@outlook.com>
2026-06-30 22:23:50 +0100

    Add a11y settings shell route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import AccessibilityShell from "./components/accessibility/AccessibilityShell";
 import AcceptStaffInvite from "./pages/AcceptStaffInvite";
+import A11ySettings from "./pages/AccessibilitySettings";
 import CustomerStaff from "./pages/CustomerStaff";
 import PreviousJobs from "./pages/PreviousJobs";
 import AppRouter from "./routes/AppRouter";
@@ -7,17 +8,10 @@ import AppRouter from "./routes/AppRouter";
 function App() {
   let content = <AppRouter />;
 
-  if (window.location.pathname === "/accept-staff-invite") {
-    content = <AcceptStaffInvite />;
-  }
-
-  if (window.location.pathname === "/team") {
-    content = <CustomerStaff />;
-  }
-
-  if (window.location.pathname === "/job-history") {
-    content = <PreviousJobs />;
-  }
+  if (window.location.pathname === "/accept-staff-invite") content = <AcceptStaffInvite />;
+  if (window.location.pathname === "/team") content = <CustomerStaff />;
+  if (window.location.pathname === "/job-history") content = <PreviousJobs />;
+  if (window.location.pathname === "/settings/accessibility") content = <A11ySettings />;
 
   return <AccessibilityShell>{content}</AccessibilityShell>;
 }
