e47b413ed5712ed15426ada1c9a11668be4f7995
CqzShepie <sheppardthomashenry@outlook.com>
2026-06-30 10:19:45 +0100

    Render accept page path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import AcceptStaffInvite from "./pages/AcceptStaffInvite";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
-    return <AppRouter />;
+  if (window.location.pathname === "/accept-staff-invite") {
+    return <AcceptStaffInvite />;
+  }
+
+  return <AppRouter />;
 }
 
 export default App;
